9ac8f1f6a931df523004120ea9db4fb7484c348c
LourInf <lourdes.infante@gmail.com>
2022-12-26 22:29:28 +0100

    Added relevant icon from API

--- src/app.js
+++ src/app.js
@@ -52,6 +52,15 @@ function showTemperature(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute(
+    "alt",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`
+  );
 }
 
 function searchCity(city) {
