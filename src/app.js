1349284dd48b349c5b86677ccac1df833ef96b25
LourInf <lourdes.infante@gmail.com>
2022-12-30 23:12:11 +0100

    Added forecast API

--- src/app.js
+++ src/app.js
@@ -42,6 +42,58 @@ today.innerHTML = `${day}, ${date} ${month}`;
 let todayTime = document.querySelector("#last-updated-currentTime");
 todayTime.innerHTML = `Last updated: ${hour}:${minutes}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="80"
+        />
+        <div>
+          <span class="temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "ce144f0cf51fa43f03431f0488a36728";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showTemperature(response) {
   //console.log(response);
   document.querySelector("#city-input").innerHTML = response.data.name;
@@ -63,10 +115,11 @@ function showTemperature(response) {
   );
 
   celsiusTemperature = response.data.main.temp;
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
-  let apiKey = "6f9e9856e7cd0b15541eb1cfd3f417ee";
+  let apiKey = "ce144f0cf51fa43f03431f0488a36728";
   let units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
